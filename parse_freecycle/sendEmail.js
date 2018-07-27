5e60dafb2ff15a1c22678b06b4b28fe837fedfe4
Xiao-Qiao Zhou <github@cssherry.com>
2018-07-27 17:09:44 +0100

    Add more things to update

--- parse_freecycle/sendEmail.js
+++ parse_freecycle/sendEmail.js
@@ -4,7 +4,7 @@ var sheetsToUpdate = [
 ];
 var sheetsToEmail = {
 };
-var searchTerms = new RegExp(' cat | pet | keyboard | yoga | plant ');
+var searchTerms = new RegExp(' cat | pet | keyboard | yoga | plant | shredder | knee');
 var maxPrevious = 60;
 var contextValues = {};
 var fetchPayload = {
