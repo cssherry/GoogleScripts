c79fe9e648a4459d1ebe7bbea3a7362757dcb4cc
cssherry <github@cssherry.com>
2022-09-26 18:38:54 +0100

    Better includes check

--- childcare/pullEvents.js
+++ childcare/pullEvents.js
@@ -32,7 +32,7 @@ function pullAndUpdateEvents() {
     const currDate = new Date();
     const currentHour = currDate.getHours();
     const weekNum = currDate.getDay();
-    if (currentHour < 8 || currentHour >= 20 || weekNum in [0, 6]) {
+    if (currentHour < 8 || currentHour >= 20 || [0, 6].includes(weekNum)) {
       return;
     }
 
